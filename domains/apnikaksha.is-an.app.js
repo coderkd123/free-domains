c800392c1dd050a06b196398752affcf732ad602
coderkd123 <124048609+coderkd123@users.noreply.github.com>
2023-06-28 23:35:52 +0530

    Create apnikaksha.is-an.app.js

---
+++ domains/apnikaksha.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'A website for my personal project',
+  domain: 'is-an.app',
+  subdomain: 'apnikaksha',
+  owner: {
+    email: 'coderkd@proton.me',
+  },
+  record: {
+    CNAME: 'apnikaksha.onrender.com'
+  },
+  proxy: false
+});
